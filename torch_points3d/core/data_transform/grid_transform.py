01f1a29873ee43c710e7f1369b7355257afab882
obarads <id115561@gmail.com>
2021-07-06 18:48:56 +0000

    update transform

--- torch_points3d/core/data_transform/grid_transform.py
+++ torch_points3d/core/data_transform/grid_transform.py
@@ -229,3 +229,216 @@ class ElasticDistortion:
         return "{}(apply_distorsion={}, granularity={}, magnitude={})".format(
             self.__class__.__name__, self._apply_distorsion, self._granularity, self._magnitude,
         )
+
+class BlockSampling(object):
+    """
+    block sampling for BlockMargining
+    """
+
+    def __init__(self, num_point, block_size=1.0, stride=1.0, mode="mean", verbose=False):
+        self._block_size = block_size
+        self._stride = stride
+        self._mode = mode
+        if verbose:
+            log.warning(
+                "If you need to keep track of the position of your points, use SaveOriginalPosId transform before using GridSampling3D"
+            )
+
+            if self._mode == "last":
+                log.warning(
+                    "The tensors within data will be shuffled each time this transform is applied. Be careful that if an attribute doesn't have the size of num_points, it won't be shuffled"
+                )
+
+    def _process(self, data):
+        if self._mode == "last":
+            data = shuffle_data(data)
+
+        coords = torch.round((data.pos) / self._grid_size)
+        if "batch" not in data:
+            cluster = grid_cluster(coords, torch.tensor([1, 1, 1]))
+        else:
+            cluster = voxel_grid(coords, data.batch, 1)
+        cluster, unique_pos_indices = consecutive_cluster(cluster)
+
+        data = group_data(data, cluster, unique_pos_indices, mode=self._mode)
+        if self._quantize_coords:
+            data.coords = coords[unique_pos_indices].int()
+
+        data.grid_size = torch.tensor([self._grid_size])
+        return data
+
+    def __call__(self, data):
+        if isinstance(data, list):
+            data = [self._process(d) for d in data]
+        else:
+            data = self._process(data)
+        return data
+
+    def __repr__(self):
+        return "{}(grid_size={}, quantize_coords={}, mode={})".format(
+            self.__class__.__name__, self._grid_size, self._quantize_coords, self._mode
+        )
+
+
+def sample_data_label(data, label, inslabel, num_sample):
+    new_data, sample_indices = sample_data(data, num_sample)
+    new_label = label[sample_indices]
+    new_inslabel = inslabel[sample_indices]
+    return new_data, new_label, new_inslabel
+
+
+def sample_data(data, num_sample):
+    """ data is in N x ...
+        we want to keep num_samplexC of them.
+        if N > num_sample, we will randomly keep num_sample of them.
+        if N < num_sample, we will randomly duplicate samples.
+    """
+    N = data.shape[0]
+    if (N == num_sample):
+        return data, range(N)
+    elif (N > num_sample):
+        sample = np.random.choice(N, num_sample)
+        return data[sample, ...], sample
+    else:
+        sample = np.random.choice(N, num_sample-N)
+        dup_data = data[sample, ...]
+        # fix range bug for python3 (warry): range(N)->list(range(N))
+        return np.concatenate([data, dup_data], 0), list(range(N))+list(sample)
+
+
+def room2blocks(data, label, inslabel, num_point, block_size=1., stride=1.,
+                random_sample=False, sample_num=None, sample_aug=1):
+
+    """ Prepare block training data.
+    Args:
+        data: N x 6 numpy array, 012 are XYZ in meters, 345 are RGB in [0,1]
+            assumes the data is shifted (min point is origin) and aligned
+            (aligned with XYZ axis)
+        label: N size uint8 numpy array from 0-12
+        num_point: int, how many points to sample in each block
+        block_size: float, physical size of the block in meters
+        stride: float, stride for block sweeping
+        random_sample: bool, if True, we will randomly sample blocks in the room
+        sample_num: int, if random sample, how many blocks to sample
+            [default: room area]
+        sample_aug: if random sample, how much aug
+    Returns:
+        block_datas: K x num_point x 6 np array of XYZRGB, RGB is in [0,1]
+        block_labels: K x num_point x 1 np array of uint8 labels
+        block_labels: K x num_point x 1 np array of uint8 labels
+
+    TODO: for this version, blocking is in fixed, non-overlapping pattern.
+    """
+    limit = np.amax(data, 0)[0:3]
+
+    # Get the corner location for our sampling blocks
+    xbeg_list = []
+    ybeg_list = []
+    if not random_sample:
+        num_block_x = int(np.ceil((limit[0] - block_size) / stride)) + 1
+        num_block_y = int(np.ceil((limit[1] - block_size) / stride)) + 1
+        for i in range(num_block_x):
+            if i % 2 == 0:
+                for j in range(num_block_y):
+                    xbeg_list.append(i*stride)
+                    ybeg_list.append(j*stride)
+            else:
+                for j in range(num_block_y)[::-1]:
+                    xbeg_list.append(i*stride)
+                    ybeg_list.append(j*stride)
+
+    else:
+        num_block_x = int(np.ceil(limit[0] / block_size))
+        num_block_y = int(np.ceil(limit[1] / block_size))
+        if sample_num is None:
+            sample_num = num_block_x * num_block_y * sample_aug
+        for _ in range(sample_num):
+            xbeg = np.random.uniform(-block_size, limit[0]) 
+            ybeg = np.random.uniform(-block_size, limit[1]) 
+            xbeg_list.append(xbeg)
+            ybeg_list.append(ybeg)
+
+    # Collect blocks
+    block_data_list = []
+    block_label_list = []
+    block_inslabel_list = []
+    idx = 0
+    for idx in range(len(xbeg_list)): 
+        xbeg = xbeg_list[idx]
+        ybeg = ybeg_list[idx]
+        xcond = (data[:, 0] <= xbeg+block_size) & (data[:, 0] >= xbeg)
+        ycond = (data[:, 1] <= ybeg+block_size) & (data[:, 1] >= ybeg)
+        cond = xcond & ycond
+        if np.sum(cond) < 100:  # discard block if there are less than 100 pts.
+            continue
+
+        block_data = data[cond, :]
+        block_label = label[cond]
+        block_inslabel = inslabel[cond]
+
+        # randomly subsample data
+        block_data_sampled, block_label_sampled, block_inslabel_sampled = \
+            sample_data_label(block_data, block_label, block_inslabel, num_point)
+        block_data_list.append(np.expand_dims(block_data_sampled, 0))
+        block_label_list.append(np.expand_dims(block_label_sampled, 0))
+        block_inslabel_list.append(np.expand_dims(block_inslabel_sampled, 0))
+
+    return np.concatenate(block_data_list, 0), \
+        np.concatenate(block_label_list, 0),\
+        np.concatenate(block_inslabel_list, 0)
+
+
+def room2blocks_plus_normalized(data, label, inslabel, num_point,
+                                block_size=1.0, stride=1.0, random_sample=False,
+                                sample_num=None, sample_aug=1):
+    """
+    Parameters
+    ----------
+    data: np.ndarry (N, 6)
+        xyzrgb points in a room
+    label: np.ndarray (N)
+        semantic labels in a room
+    inslabel: np.ndarray (N)
+        instance labels in a room
+    num_point: int
+        number of points in a block
+    block_size: float
+        block size
+    stride: float
+        stride for block sweeping
+    random_sample: bool
+        if True, we will randomly sample blocks in the room
+    sample_num: int 
+        if random sample, how many blocks to sample [default: None]
+    sample_aug: 
+        if random sample, how much aug
+
+    Returns
+    -------
+    new_data_batch: np.ndarray (K, num_point, 9) 
+        np array of XYZRGBnormalizedXYZ, RGB is in [0,1]
+    label_batch: np.ndarray (K, num_point) 
+        np array of uint8 labels
+    inslabel_batch: np.ndarray (K, num_point) 
+        np array of uint8 labels
+    """
+
+    max_room_x = max(data[:,0])
+    max_room_y = max(data[:,1])
+    max_room_z = max(data[:,2])
+    
+    data_batch, label_batch, inslabel_batch = room2blocks(
+        data, label, inslabel, num_point, block_size, stride, random_sample, 
+        sample_num, sample_aug)
+
+    new_data_batch = np.zeros((data_batch.shape[0], num_point, 9))
+    for b in range(data_batch.shape[0]):
+        new_data_batch[b, :, 6] = data_batch[b, :, 0]/max_room_x
+        new_data_batch[b, :, 7] = data_batch[b, :, 1]/max_room_y
+        new_data_batch[b, :, 8] = data_batch[b, :, 2]/max_room_z
+        minx = min(data_batch[b, :, 0])
+        miny = min(data_batch[b, :, 1])
+        data_batch[b, :, 0] -= (minx+block_size/2)
+        data_batch[b, :, 1] -= (miny+block_size/2)
+    new_data_batch[:, :, 0:6] = data_batch
+    return new_data_batch, label_batch, inslabel_batch
